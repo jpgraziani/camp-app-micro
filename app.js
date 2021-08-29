5f904c7a5e3f16ea44808483eddba1314ebb381b
Jonathan Graziani <jonathangraziani@Jonathans-MacBook-Air.local>
2021-08-29 15:05:26 -0700

    post and put added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 
 // const db = mongoose.connection;
@@ -19,15 +20,45 @@ mongoose.connect(process.env.CAMPGROUND_URI, { useNewUrlParser: true, useUnified
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//we need to parse the body when we want a return on http method Post
+app.use(express.urlencoded({ extended: true}))
+//override date from edit
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({title: 'My Backyard', description: 'cheap spot'})
-  await camp.save();
-  res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds/index', { campgrounds })
+})
+
+//order matters here. new needs to be first
+app.get('/campgrounds/addCampground', (req, res) => {
+  res.render('campgrounds/addCampground')
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground })
+})
+
+app.get('/campgrounds/:id/editCampground', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/editCampground', { campground })
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.listen(3000, () => {
